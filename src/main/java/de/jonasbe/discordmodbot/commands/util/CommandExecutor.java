8b95c49917ffa98ed67b356819e536588502ea9e
jobender <jbender@united-internet.de>
2022-10-06 10:37:15 +0200

    Deleted old code and setup JavaScript project

--- src/main/java/de/jonasbe/discordmodbot/commands/util/CommandExecutor.java
+++
@@ -1,7 +0,0 @@
-package de.jonasbe.discordmodbot.commands.util;
-
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-
-public interface CommandExecutor {
-    void onCommand(MessageReceivedEvent event, String[] args);
-}
